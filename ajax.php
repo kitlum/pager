3918ad465474af27f0e0e604555c7bb94d55b604
Kitlum <kitlum@ukr.net>
2015-10-15 23:24:04 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,9 +1,13 @@
-<? php
-$a=json_decode($_POST['data']);
+ <?php
+ $a=array("action"=>"find_data", "request"=>array("currentPage"=>2, "pcsPerPage"=>5, "shownPages"=>3, "ascending"=>TRUE, "tableName"=>"pager", "sorter"=>"id"));
+/*$a=json_decode($_POST['data']);*/
 if ($a['action']=='find_data'){
-	include conditioner.class.php;
-	$b=new Condiitioner;
-	$b->getData($a);
+	include "conditioner.class.php";
+	$b=new Wrapper;
+	foreach ($a["request"] as $key=>$value)
+	{$b->$key=$value;}
 }
-$output=json_encode($b);
-return ($output)
+$output=$b->get_data();
+/*$output=json_encode($b);
+return ($output)*/
+print_r($output);
